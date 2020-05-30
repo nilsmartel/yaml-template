6d6e778ba691ad7c6938e592680e587b3b900933
Nils Martel <nils.martel@graphmasters.net>
2020-05-30 17:15:40 +0200

    Include node modules

--- index.ts
+++ index.ts
@@ -3,5 +3,15 @@
 import * as commander from 'commander'
 
 function main(args: string[]) {
+  let config = commander
+      .description(
+          'Create obvious, powerfull and easily maintainable yaml templates')
+      .option('-f, --file', 'Input file')
+      .option('-o, --output', 'Output file')
+      .parse(args)
 
+  console.log(config.input)
+  console.log(config.output)
 }
+
+main(process.argv)
